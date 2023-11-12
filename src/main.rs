d840e73f8c8110ee66a2ede1199d011fe80ad2cb
Laurie Ibbs <laurie.ibbs@gmail.com>
2023-11-12 21:41:53 +0000

    rewrite main as hello world

--- src/main.rs
+++ src/main.rs
@@ -4,36 +4,25 @@
 
 use panic_halt as _;
 
-use stm32f3xx_hal as hal;
+//use stm32f3xx_hal as hal;
 
 use cortex_m_rt::entry;
-use hal::pac;
-use hal::prelude::*;
+use cortex_m_semihosting::{debug,hprintln};
+//use hal::pac;
+//use hal::prelude::*;
 
 #[entry]
 fn main() -> ! {
-    let dp = pac::Peripherals::take().unwrap();
+    //this is taken from Rust Embedded book getting started template
+    //as a hello world for reference
+    //do setup here
+    hprintln!("Hello, world!");
 
-    let mut rcc = dp.RCC.constrain();
-    let mut gpioe = dp.GPIOE.split(&mut rcc.ahb);
-
-    let mut led = gpioe
-        .pe13
-        .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
-
-    led.set_low().unwrap();
+    // exit QEMU
+    // NOTE do not run this on hardware; it can corrupt OpenOCD state
+    debug::exit(debug::EXIT_SUCCESS);
 
     loop {
-        led.toggle().unwrap();
-        cortex_m::asm::delay(8_000_000);
-        // Toggle by hand.
-        // Uses `StatefulOutputPin` instead of `ToggleableOutputPin`.
-        // Logically it is the same.
-        if led.is_set_low().unwrap() {
-            led.set_high().unwrap();
-        } else {
-            led.set_low().unwrap();
-        }
-        cortex_m::asm::delay(8_000_000);
+        //do repeating work here
     }
 }
